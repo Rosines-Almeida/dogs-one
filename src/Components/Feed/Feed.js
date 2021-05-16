a6410f5386386a1c9cb1752e4db9507edbf6bece
Rosinês da Silva Almeida <r.da.silva.almeida@avanade.com>
2021-05-16 19:26:20 -0300

    ajuste de layout

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -3,48 +3,48 @@ import { FeedModal } from './FeedModal'
 import { FeedPhoto } from './FeedPhoto'
 import PropTypes from 'prop-types'
 
-export const Feed = ({user}) => {
-    const [modalPhoto, setModalPhoto ] = React.useState(null);
-    const [pages, setPages ] = React.useState([1]);
-    const [infinite, setInfinite] = React.useState(true)
+export const Feed = ({ user }) => {
+  const [modalPhoto, setModalPhoto] = React.useState(null);
+  const [pages, setPages] = React.useState([1]);
+  const [infinite, setInfinite] = React.useState(true)
 
-    React.useEffect(()=>{
-        let wait = false;
-        function infiniteScroll() {
-            if (infinite) {
-              const scroll = window.scrollY;
-              const height = document.body.offsetHeight - window.innerHeight;
-              if (scroll > height * 0.75 && !wait) {
-                setPages((pages) => [...pages, pages.length + 1]);
-                wait = true;
-                setTimeout(() => {
-                  wait = false;
-                }, 500);
-              }
-            }
-          }
+  React.useEffect(() => {
+    let wait = false;
+    function infiniteScroll() {
+      if (infinite) {
+        const scroll = window.scrollY;
+        const height = document.body.offsetHeight - window.innerHeight;
+        if (scroll > height * 0.75 && !wait) {
+          setPages((pages) => [...pages, pages.length + 1]);
+          wait = true;
+          setTimeout(() => {
+            wait = false;
+          }, 500);
+        }
+      }
+    }
 
-          window.addEventListener('wheel', infiniteScroll);
-          window.addEventListener('scroll', infiniteScroll);
-          return () => {
-            window.removeEventListener('wheel', infiniteScroll);
-            window.removeEventListener('scroll', infiniteScroll);
-          };
-        }, [infinite]);
-    return (
-        <div>
-          {modalPhoto && <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto}/> }
-            {pages.map((page) => (
-                <FeedPhoto key={page} page={page} user={user} setModalPhoto={setModalPhoto} setInfinite={setInfinite} />
-            ))}
+    window.addEventListener('wheel', infiniteScroll);
+    window.addEventListener('scroll', infiniteScroll);
+    return () => {
+      window.removeEventListener('wheel', infiniteScroll);
+      window.removeEventListener('scroll', infiniteScroll);
+    };
+  }, [infinite]);
+  return (
+    <>
+      {modalPhoto && <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />}
+      {pages.map((page) => (
+        <FeedPhoto key={page} page={page} user={user} setModalPhoto={setModalPhoto} setInfinite={setInfinite} />
+      ))}
 
-        </div>
-    )
+    </>
+  )
 }
-Feed.defaultProps={
+Feed.defaultProps = {
   user: 0
 };
-Feed.prototypes ={
+Feed.prototypes = {
   user: PropTypes.oneOfType([
     PropTypes.string.isRequired,
     PropTypes.number.isRequired
--- src/Components/Helper/Head.js
+++ src/Components/Helper/Head.js
@@ -1,14 +1,12 @@
 import React from 'react'
 
 export const Head = (props) => {
-    React.useEffect(()=>{
-        document.title = props.title+ ' | Dogs'
+    React.useEffect(() => {
+        document.title = props.title + ' | Dogs'
         document.querySelector("meta[name='description']")
-        .setAttribute('content', props.description || '')
+            .setAttribute('content', props.description || '')
     })
     return (
-        <div>
-            
-        </div>
+        <></>
     )
 }
